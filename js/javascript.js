0785093537f35e31b07dcc2e894502eb9537d85d
SnowNooDLe <sdw123123@gmail.com>
2018-11-08 14:27:05 +1300

    changing variable name

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -43.5368, lng: 172.6399};
-  // The map, centered at Uluru
+  // The location of NZMUsicLive
+  var NZMUsicLive = {lat: -43.5368, lng: 172.6399};
+  // The map, centered at NZMUsicLive
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 19, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+      document.getElementById('map'), {zoom: 19, center: NZMUsicLive});
+  // The marker, positioned at NZMUsicLive
+  var marker = new google.maps.Marker({position: NZMUsicLive, map: map});
 }
